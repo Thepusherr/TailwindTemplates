223599fdab580d8a3418a1c66e93700b150dc143
Pusher <spunkspunkik@gmail.com>
2024-08-14 21:08:12 +0300

    fixing image display

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,9 @@
 //= link tailwind.css
+//= link application.css
+//= link application.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module TailwindTemplates
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.paths << Rails.root.join('app', 'assets', 'home1')
+
+    Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
+      config.assets.paths << path
+    end
   end
 end
